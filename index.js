fa5c1bf5579e2db92682c1d412d8755bcfdfa5f4
cameronotoole44 <cameronotoole404@gmail.com>
2024-04-09 19:14:57 -0400

    repo to github

---
+++ index.js
@@ -0,0 +1,8 @@
+// home page outside of library- grass, stone, pathway(?)
+// user clicks building to enter
+// new layout is inside the library bookshelves, fireplace, chairs, tables, etc
+// MAYBE user moves character (keep it simple for mobile in mind) to bookshelf
+// user clicks on bookshelf to open their personal library
+// third page is the actual interactive library 
+
+
